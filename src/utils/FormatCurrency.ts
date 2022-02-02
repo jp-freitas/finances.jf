c5f43c5c1539bde3eb5a825bf068a0d7d63b40b6
TecJP <tec.joao.freitas@gmail.com>
2022-02-02 16:25:40 -0300

    Changing the variables name.

--- src/utils/FormatCurrency.ts
+++ src/utils/FormatCurrency.ts
@@ -1,8 +1,8 @@
-export function FormatCurrency(price: number) {
+export function FormatCurrency(amount: number) {
   const formattedCurrency = Intl.NumberFormat('pt-BR', {
     style: 'currency',
     maximumFractionDigits: 2,
     currency: 'BRL'
-  }).format(price);
+  }).format(amount);
   return formattedCurrency;
 }
