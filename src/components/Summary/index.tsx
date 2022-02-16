99584ae0a48948a10ebb2dde47af45de8649c20c
TecJP <tec.joao.freitas@gmail.com>
2022-02-16 07:36:21 -0300

    Adding the transaction context.

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -7,7 +7,7 @@ import { TransactionsContext } from '../../contexts/TransactionsContext';
 import { Container } from "./styles";
 
 export function Summary() {
-  const transactions = useContext(TransactionsContext);
+  const { transactions } = useContext(TransactionsContext);
 
   return (
     <Container>
