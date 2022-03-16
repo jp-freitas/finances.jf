e32afbaf219809136235847780c184191bd28900
TecJP <tec.joao.freitas@gmail.com>
2022-03-16 15:03:15 -0300

    Making the TransactionsTable responsible

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -4,36 +4,63 @@ import { format } from 'date-fns';
 import { TransactionsContext } from '../../contexts/TransactionsContext';
 import { FormatCurrency } from '../../utils/FormatCurrency';
 import { Container } from "./styles";
+import { useWidth } from '../../hooks/useWidth';
 
 export function TransactionsTable() {
+  const { width } = useWidth();
   const { transactions } = useContext(TransactionsContext);
 
   return (
     <Container>
-      <table>
-        <thead>
-          <tr>
-            <th>Título</th>
-            <th>Valor</th>
-            <th>Categoria</th>
-            <th>Data</th>
-          </tr>
-        </thead>
-        <tbody>
-          {transactions.map(transaction => (
-            <tr key={transaction.id}>
-              <td>{transaction.title}</td>
-              <td className={transaction.type}>{
-                transaction.type === 'withdraw' ?
-                  '-' + FormatCurrency(transaction.amount) :
-                  FormatCurrency(transaction.amount)
-              }</td>
-              <td>{transaction.category}</td>
-              <td>{format(new Date(transaction.created_at), 'dd/MM/yyyy')}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      {
+        width >= 625 ?
+          <table>
+            <thead>
+              <tr>
+                <th>Título</th>
+                <th>Valor</th>
+                <th>Categoria</th>
+                <th>Data</th>
+              </tr>
+            </thead>
+            <tbody>
+              {transactions.map(transaction => (
+                <tr key={transaction.id}>
+                  <td>{transaction.title}</td>
+                  <td className={transaction.type}>{
+                    transaction.type === 'withdraw' ?
+                      '-' + FormatCurrency(transaction.amount) :
+                      FormatCurrency(transaction.amount)
+                  }</td>
+                  <td>{transaction.category}</td>
+                  <td>{format(new Date(transaction.created_at), 'dd/MM/yyyy')}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          :
+          <>
+            <div className='header-group'>
+              <h1>Listagem</h1>
+              <p>{`${transactions.length > 1 ? transactions.length + ' itens' : transactions.length + ' item'}`}</p>
+            </div>
+            {transactions.map(transaction => (
+              <div key={transaction.id} className="card">
+                <p className='header'>{transaction.title}</p>
+                <p className={`${transaction.type} content`}>
+                  {transaction.type === 'withdraw' ?
+                    '-' + FormatCurrency(transaction.amount) :
+                    FormatCurrency(transaction.amount)
+                  }
+                </p>
+                <div className="footer">
+                  <p>{transaction.category}</p>
+                  <p>{format(new Date(transaction.created_at), 'dd/MM/yyyy')}</p>
+                </div>
+              </div>
+            ))}
+          </>
+      }
     </Container>
   );
 }
--- src/components/TransactionsTable/styles.ts
+++ src/components/TransactionsTable/styles.ts
@@ -39,4 +39,62 @@ export const Container = styled.div`
     }
   }
 
+  .header-group {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    h1 {
+      color: var(--text-title);
+      font-size: 1.5rem;
+    }
+
+    p {
+      color: var(--text-title);
+      font-size: 1.2rem;
+    }
+  }
+
+  .card {
+    display: flex;
+    flex-direction: column;
+    background: var(--shape);
+    width: 100%;
+    height: 100%;
+    margin-top: 1rem;
+    border-radius: 0.8rem;
+    padding: 0 0.5rem;
+
+    &:last-child {
+      margin-bottom: 1rem;
+    }
+
+    .header {
+      padding: 0.5rem 0.5rem;
+      color: var(--text-title);
+      font-size: 1.5rem;
+    }
+
+    .content {
+      padding: 0.5rem 0.5rem;
+      font-size: 1.5rem;
+
+      &.withdraw {
+        color: var(--red);
+      }
+
+      &.deposit {
+        color: var(--green)
+      }
+    }
+
+    .footer {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      padding: 0.5rem 0.5rem;
+      color: var(--text-body);
+      font-size: 1.5rem;
+    }
+  }
 `;
